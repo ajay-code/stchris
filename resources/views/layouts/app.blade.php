30aaa4533ca94617af68dd048ac9215f9440355a
Ajay Singh <ajay10mar96@gmail.com>
2017-12-06 15:35:02 +0530

    changing asset url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	<title>StChrisCloud</title>
-	<link rel=icon href="/storage/{{setting('site.logo')}}">
-	<link rel="stylesheet" href="/css/app.css">
+	<link rel=icon href="{{url('/storage')}}/{{setting('site.logo')}}">
+	<link rel="stylesheet" href="{{url('css/app.css')}}">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 	<style>
 		.bg-primary {
@@ -34,7 +34,7 @@
 	<div class="container-fluid no-padding">
 		@include('layouts.partials.nav') @yield('content')
 	</div>
-	<script src="/js/app.js"></script>
+	<script src="{{url('/js/app.js')}}"></script>
 </body>
 
 </html>
